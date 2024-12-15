96931723a5d6c92caf7e3b1bcfed38ea42998e31
Aidan Venter <aidanbv03@gmail.com>
2024-12-15 12:27:56 +0200

    Improve install

--- virtuos/setup.py
+++ virtuos/setup.py
@@ -5,5 +5,4 @@ import os
 cli = ["btop", "curl", "eza", "fzf", "git", "neofetch", "net-tools", "rar", "unrar", "unzip", "zip"]
 desktop = ["gparted"]
 
-for app in cli + desktop:
-    os.system(f"sudo apt install -y {app}")
+os.system(f"sudo apt install -y {' '.join(cli + desktop)}")
