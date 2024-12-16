b308c054469da3683e3eb287147bb04bf2a3effd
Aidan Venter <aidanbv03@gmail.com>
2024-12-16 14:43:05 +0200

    Add gnome-shell-extension-manager

--- virtuos/setup.py
+++ virtuos/setup.py
@@ -3,6 +3,6 @@ import os
 
 os.system("sudo apt update")
 
-desktop = ["dconf-editor", "gnome-tweaks", "gparted"]
+desktop = ["dconf-editor", "gnome-shell-extension-manager", "gnome-tweaks", "gparted"]
 
 os.system(f"sudo apt install -y {' '.join(desktop)}")
