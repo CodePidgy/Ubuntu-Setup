8168db6c0dc97c5b0b62759714ae94c7e177d4ad
Aidan Venter <aidanbv03@gmail.com>
2025-01-19 21:31:00 +0200

    Add more calls

--- virtuos/setup.py
+++ virtuos/setup.py
@@ -7,3 +7,6 @@ subprocess.call(["sudo", "apt", "upgrade"])
 desktop = ["dconf-editor", "gnome-shell-extension-manager", "gnome-tweaks", "gparted"]
 
 subprocess.call(["sudo", "apt", "install", "-y"] + desktop)
+subprocess.call(["python3", "virtuos/terminal.py"])
+subprocess.call(["stow", "-d", "dotfiles", "-t", "~/"])
+subprocess.call(["python3", "virtuos/gnome.py"])
