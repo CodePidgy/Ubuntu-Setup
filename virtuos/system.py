fcff519a01de9c28edce753ef69615476d42a743
Aidan Venter <aidanbv03@gmail.com>
2025-01-21 16:46:28 +0200

    Add daemon-reload

--- virtuos/system.py
+++ virtuos/system.py
@@ -9,6 +9,7 @@ print_subheading("Update")
 subprocess.call(["sudo", "apt", "update"])
 subprocess.call(["sudo", "apt", "upgrade", "-y"])
 subprocess.call(["sudo", "apt", "autoremove", "-y"])
+subprocess.call(["sudo", "systemctl", "daemon-reload"])
 
 print_subheading("System Apps")
 desktop = ["dconf-editor", "gnome-shell-extension-manager", "gnome-tweaks", "gparted"]
