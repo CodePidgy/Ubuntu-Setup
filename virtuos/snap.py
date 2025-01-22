6b0d5067fba4e0848b533e6115584d5e10d3ae33
Aidan Venter <aidanbv03@gmail.com>
2025-01-22 22:53:29 +0200

    Add Spotify to favourite apps

--- virtuos/snap.py
+++
@@ -1,8 +0,0 @@
-# system imports --------------------------------------------------------------------------------- #
-import os
-
-# script ----------------------------------------------------------------------------------------- #
-apps = [["code", True], ["spotify", False], ["thunderbird", False]]
-
-for app in apps:
-    os.system(f"sudo snap install {'--classic' if app[1] else ''} {app[0]}")
