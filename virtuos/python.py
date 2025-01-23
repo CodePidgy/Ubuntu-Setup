dfe82f4cb772f33f26e0e1c45e5a4ed61de6d0fc
Aidan Venter <aidanbv03@gmail.com>
2025-01-23 15:12:15 +0200

    Add poetry setup

--- virtuos/python.py
+++ virtuos/python.py
@@ -11,8 +11,8 @@ os.system("curl -fsSL https://pyenv.run | bash")
 print_subheading("Build Dependencies")
 os.system(
     "sudo apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev "
-    "libsqlite3-dev curl git libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev "
-    "liblzma-dev"
+    "libsqlite3-dev curl git libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev "
+    "libffi-dev liblzma-dev"
 )
 
 print_subheading("Python")
@@ -32,3 +32,6 @@ print_subheading("Pip Tools")
 os.system("$HOME/.pyenv/shims/pipx install black")
 os.system("$HOME/.pyenv/shims/pipx install poetry")
 os.system("$HOME/.local/bin/poetry config virtualenvs.in-project true")
+
+print_subheading("Installer Environment")
+os.system("$HOME/.local/bin/poetry install")
