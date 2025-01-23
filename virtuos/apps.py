43172e4dcde702d539c9ddc491f4bd7f6cb5163e
Aidan Venter <aidanbv03@gmail.com>
2025-01-23 15:12:04 +0200

    Migrate app script calling to separate script

---
+++ virtuos/apps.py
@@ -0,0 +1,10 @@
+# system imports --------------------------------------------------------------------------------- #
+import os
+
+# local imports ---------------------------------------------------------------------------------- #
+from utils import print_heading, print_subheading
+
+# script ----------------------------------------------------------------------------------------- #
+print_heading("Apps")
+print_subheading("Snap")
+os.system("python3 virtuos/apps/snap.py")
