ca347c6a93bf3a47758f72c3c1e15bb6b694d130
Aidan Venter <aidanbv03@gmail.com>
2025-01-23 13:54:25 +0200

    Fix Python script

--- virtuos/python.py
+++ virtuos/python.py
@@ -8,10 +8,6 @@ from utils import print_subheading
 print_subheading("Pyenv")
 os.system("curl -fsSL https://pyenv.run | bash")
 
-print("Loading Pyenv...", end="")
-os.system("source ~/.zshrc")
-print("\tDone")
-
 print_subheading("Build Dependencies")
 os.system(
     "sudo apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev "
@@ -22,7 +18,7 @@ os.system(
 print_subheading("Python")
 os.system(
     "$HOME/.pyenv/bin/pyenv install $($HOME/.pyenv/bin/pyenv install -l | "
-    r"grep '^  3\.[0-9]*\.[0-9]+$' | tail -n 1)"
+    r"grep '^  3\.[0-9]*\.[0-9]*$' | tail -n 1)"
 )
 
 print("Setting Python version...", end="")
@@ -30,9 +26,9 @@ os.system("$HOME/.pyenv/bin/pyenv global $($HOME/.pyenv/bin/pyenv versions | tai
 print("\tDone")
 
 print_subheading("Pip")
-os.system("pip install pipx")
+os.system("$HOME/.pyenv/shims/pip install pipx")
 
 print_subheading("Pip Tools")
-os.system("pipx install black")
-os.system("pipx install poetry")
-os.system("poetry config virtualenvs.in-project true")
+os.system("$HOME/.pyenv/shims/pipx install black")
+os.system("$HOME/.pyenv/shims/pipx install poetry")
+os.system("$HOME/.local/bin/poetry config virtualenvs.in-project true")
