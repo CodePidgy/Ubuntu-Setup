fa0337e09e5b22b26c43c88caebb361c22c94c06
Aidan Venter <aidanbv03@gmail.com>
2025-01-23 15:56:30 +0200

    Add Postman

--- virtuos/apps/snap.py
+++ virtuos/apps/snap.py
@@ -2,7 +2,7 @@
 import os
 
 # script ----------------------------------------------------------------------------------------- #
-apps = [["code", True], ["spotify", False], ["thunderbird", False]]
+apps = [["code", True], ["postman", False], ["spotify", False], ["thunderbird", False]]
 
 for app in apps:
     os.system(f"sudo snap install {'--classic' if app[1] else ''} {app[0]}")
