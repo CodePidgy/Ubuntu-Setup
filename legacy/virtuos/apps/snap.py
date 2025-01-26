3ec2b9e48e184b7afcfd29659f3017809a7b10d3
Aidan Venter <aidanbv03@gmail.com>
2025-01-26 16:32:04 +0200

    Remove legacy code

--- legacy/virtuos/apps/snap.py
+++
@@ -1,8 +0,0 @@
-# system imports --------------------------------------------------------------------------------- #
-import os
-
-# script ----------------------------------------------------------------------------------------- #
-apps = [["code", True], ["postman", False], ["spotify", False], ["thunderbird", False]]
-
-for app in apps:
-    os.system(f"sudo snap install {'--classic' if app[1] else ''} {app[0]}")
