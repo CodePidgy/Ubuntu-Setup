e60467cfaea0c94b34ec5b35533d27035a36c9bf
Aidan Venter <aidanbv03@gmail.com>
2025-01-27 22:34:47 +0200

    Add sudo to chsh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ sudo apt install -y btop curl eza fzf git rar stow unrar unzip zsh
 sudo rm -f /usr/share/applications/btop.desktop
 
 # Set default shell
-chsh -s /usr/bin/zsh
+sudo chsh -s /usr/bin/zsh
 
 # Install some basic apps
 sudo apt install -y dconf-editor gnome-shell-extension-manager gnome-tweaks
