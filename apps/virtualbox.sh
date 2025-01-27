4fc265a0d3abf3f4f31346d71d0b04cffb3be4cf
Aidan Venter <aidanbv03@gmail.com>
2025-01-27 16:08:38 +0200

    Fix Virtualbox script getting stuck at PUEL acceptance

--- apps/virtualbox.sh
+++ apps/virtualbox.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Preset the PUEL license acceptance
+echo virtualbox-ext-pack virtualbox-ext-pack/license select true | sudo debconf-set-selections
+
+# Install VirtualBox
 sudo apt install -y virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso
