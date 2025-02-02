b7568eac4c866e46f282d9937daa835a676c8543
Aidan Venter <aidanbv03@gmail.com>
2025-02-02 16:05:38 +0200

    Fix profile file not pointing to correct profile picture

--- setup.sh
+++ setup.sh
@@ -43,6 +43,7 @@ dconf load / < dconf/system.ini
 # Set profile picture
 sudo rm -f /var/lib/AccountsService/icons/$USER
 sudo cp images/profile.png /var/lib/AccountsService/icons/$USER
+sudo sed -i "s/Icon=\/home\/aidan\/.face/Icon=\/var\/lib\/AccountsService\/icons\/aidan/" /var/lib/AccountsService/users/aidan
 
 # Set wallpaper
 mkdir $HOME/Pictures/Wallpapers
