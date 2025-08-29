d4632fb7e7631110f494f5a752b34d6c9c28017f
Aidan Venter <aidanbv03@gmail.com>
2025-08-30 00:11:59 +0200

    feat: add rider script for config stuff

---
+++ apps/installers/rider.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Create file
+sudo touch /etc/sysctl.d/idea.conf
+
+# Insert config
+sudo bash -c "echo 'fs.inotify.max_user_watches = 1000000' > /etc/sysctl.d/idea.conf"
+
+# Reload sysctl
+sudo sysctl -p --system
