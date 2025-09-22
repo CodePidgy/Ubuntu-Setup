2757b4b7c21c1a62b17c96dbc505107aaebb5186
Aidan Venter <aidanbv03@gmail.com>
2025-09-22 07:02:49 +0200

    feat: add ngrok

---
+++ apps/installers/ngrok.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Add ngrok repository
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
+echo "deb https://ngrok-agent.s3.amazonaws.com bookworm main" | sudo tee /etc/apt/sources.list.d/ngrok.list
+
+# Install ngrok
+sudo apt update
+sudo apt install -y ngrok
