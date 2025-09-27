70fe9f49baa46df451cdb6bb689906cd7f1099f6
Aidan Venter <aidanbv03@gmail.com>
2025-09-27 22:52:17 +0200

    feat: add desktop dconf settings

--- procedures/dconf.sh
+++ procedures/dconf.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+desktop=false
 laptop=false
+
+if [[ "$1" == "--desktop" ]]; then
+    desktop=true
+fi
 
 if [[ "$1" == "--laptop" ]]; then
     laptop=true
@@ -9,6 +14,12 @@ fi
 # Load dconf settings
 dconf load / < dconf/system.ini
 dconf load / < dconf/extensions.ini
+
+# Load desktop-specific dconf settings
+if $desktop; then
+    dconf load / < dconf/desktop/system.ini
+    dconf load / < dconf/desktop/extensions.ini
+fi
 
 # Load laptop-specific dconf settings
 if $laptop; then
