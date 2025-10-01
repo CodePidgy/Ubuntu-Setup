6594ca02ceb9bc2c51b82e9aba8b72ac60fa142b
Aidan Venter <aidanbv03@gmail.com>
2025-10-01 16:31:29 +0200

    fix: add increasing inotify watch limit

--- apps/installers/cursor.sh
+++ apps/installers/cursor.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-# Find the .deb file
+echo "[WAIT] Please download Cursor from https://cursor.com/download"
+read -p "Press Enter to continue"
+
+echo "[INFO] Finding .deb file..."
 FILE=$(find $HOME/Downloads -type f -regextype posix-extended -regex ".*/cursor_[0-9]+\.[0-9]+\.[0-9]+_amd64\.deb" | head -n 1)
 
-# Check if the file was found
 if [[ -z "$FILE" ]]; then
-    echo "No matching .deb file found in $dir"
+    echo "[ERROR] No matching .deb file found in $HOME/Downloads"
     exit 1
 fi
 
-# Install Cursor
+echo "[INFO] Installing Cursor..."
 sudo apt install -y $FILE
+
+echo "[INFO] Increasing inotify watch limit..."
+sudo cp /etc/sysctl.conf /etc/sysctl.conf.bak
+sudo bash -c "echo '# Increase inotify watch limit for Cursor' >> /etc/sysctl.conf"
+sudo bash -c "echo 'fs.inotify.max_user_watches = 524288' >> /etc/sysctl.conf"
+sudo sysctl -p --system
+
+echo "[INFO] Done"
