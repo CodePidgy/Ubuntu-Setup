0fd39d243ef089b4f75e5f9aea863bff48abb31f
Aidan Venter <aidanbv03@gmail.com>
2025-10-02 16:01:39 +0200

    fix: remove docker desktop

--- apps/installers/docker.sh
+++ apps/installers/docker.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
-echo "[INFO] Finding latest version..."
-LATEST_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-echo "[INFO] Docker Compose version: $LATEST_VERSION"
-
-echo "[INFO] Installing Docker Compose..."
-sudo curl -L "https://github.com/docker/compose/releases/download/$LATEST_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
-sudo chmod +x /usr/bin/docker-compose
-
-echo "[INFO] Adding Docker Desktop repository..."
+echo "[INFO] Adding Docker repository..."
+sudo apt install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian bookworm stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
 
-echo "[INFO] Installing Docker Desktop..."
-wget -P $HOME/Downloads https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb
-sudo apt install -y $HOME/Downloads/docker-desktop-amd64.deb
-
-echo "[INFO] Installing extras..."
-sudo apt install -y docker-ce
+echo "[INFO] Installing Docker..."
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "[INFO] Cleaning up..."
 rm -f $HOME/Downloads/docker-desktop-amd64.deb
