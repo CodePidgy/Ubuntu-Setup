c9bf3c868462bc30734fc7bf364c4c8caf522406
Aidan Venter <aidanbv03@gmail.com>
2025-10-06 08:14:47 +0200

    fix: hardware clock using utc

--- procedures/basics.sh
+++ procedures/basics.sh
@@ -5,3 +5,6 @@ stow -d dotfiles -t $HOME .
 
 # Install some basic apps
 sudo apt install -y alacritty dconf-editor gnome-shell-extension-manager gnome-tweaks
+
+# Use local time instead of UTC
+sudo timedatectl set-local-rtc 1
