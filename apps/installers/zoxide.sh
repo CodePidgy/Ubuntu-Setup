bb6fdddcbf7a289190078040e04aaa9189c85b77
Aidan Venter <aidanbv03@gmail.com>
2025-10-27 16:51:59 +0200

    fix: incorrect installation method for zoxide

--- apps/installers/zoxide.sh
+++ apps/installers/zoxide.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-echo "[INFO] Adding repository..."
-curl -fsSL https://apt.cli.rs/pubkey.asc | sudo tee -a /usr/share/keyrings/rust-tools.asc
-curl -fsSL https://apt.cli.rs/rust-tools.list | sudo tee /etc/apt/sources.list.d/rust-tools.list
-sudo apt update
-
 echo "[INFO] Installing zoxide..."
-sudo apt install -y zoxide
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
