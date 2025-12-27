eadf5f21ae006d17ccd57fbf078e73ecace257de
Aidan Venter <aidanbv03@gmail.com>
2025-12-28 01:18:14 +0200

    feat: improve trim

--- procedures/trim.sh
+++ procedures/trim.sh
@@ -5,3 +5,12 @@ sudo apt purge -y info memtest86+
 
 # Remove default software app
 sudo apt purge -y gnome-software-common gnome-software-plugin-flatpak gnome-software-plugin-snap
+
+# Remove other apps
+sudo apt purge -y gnome-characters gnome-font-viewer yelp gnome-language-selector.desktop
+
+# Remove other snaps
+sudo snap remove --purge firmware-updater
+
+# Hide shortcuts
+echo Hidden=true | sudo tee -a /usr/share/applications/software-properties-drivers.desktop
