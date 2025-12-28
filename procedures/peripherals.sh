7e8a6b9fee15635e913753b8ff9571532b5ec29c
Aidan Venter <aidanbv03@gmail.com>
2025-12-28 09:37:52 +0200

    fix: migrate solaar back to apt

--- procedures/peripherals.sh
+++ procedures/peripherals.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Install Solaar
+sudo apt install -y solaar
+
 # Set up Keychron Launcher keyboard access
 sudo touch /etc/udev/rules.d/99-keychron-launcher.rules
 echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0761", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"' | sudo tee -a /etc/udev/rules.d/99-vial.rules > /dev/null
