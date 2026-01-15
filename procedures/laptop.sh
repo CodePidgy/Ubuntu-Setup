2dd66e90e26f50446c0afc3841cab5a7dfd4d9b2
Aidan Venter <aidanbv03@gmail.com>
2026-01-15 21:07:33 +0200

    feat: change work script to laptop script

--- procedures/laptop.sh
+++ procedures/laptop.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Disable sleep on lid close
-sudo sed -i 's/#HandleLidSwitch=suspend/HandleLidSwitch=ignore/' /etc/systemd/logind.conf
-
 # Enable wake on USB
 sudo touch /etc/udev/rules.d/10-wakeup.rules
 echo 'ACTION=="add", SUBSYSTEM=="usb", ATTRS{3434}=="c548", ATTRS{d027}=="8120", ATTR{power/wakeup}="enabled"' | sudo tee -a /etc/udev/rules.d/10-wakeup.rules
