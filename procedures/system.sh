179f89f9ea2b1a48af4688d66b11e68b8c9e3ad4
Aidan Venter <aidanbv03@gmail.com>
2026-02-22 13:48:18 +0200

    feat: add grub resolution fix

--- procedures/system.sh
+++ procedures/system.sh
@@ -9,3 +9,10 @@ sudo apt autoremove -y
 
 # Use local time instead of UTC
 sudo timedatectl set-local-rtc 1
+
+# Remove memtest from grub menu
+sudo apt purge -y info memtest86+
+
+# Make grub menu 1920x1080
+sudo sed -i 's/GRUB_GFXMODE=640x480/GRUB_GFXMODE=1920x1080/' /etc/default/grub
+sudo update-grub
--- procedures/trim.sh
+++ procedures/trim.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Remove memtest from grub menu
-sudo apt purge -y info memtest86+
-
 # Remove default software app
 sudo apt purge -y gnome-software-common gnome-software-plugin-flatpak
 
